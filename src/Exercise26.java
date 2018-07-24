ec4124cb07cffbce3198812f7cb8afe6e1702a89
Jordan Parker <parkerj4321@gmail.com>
2018-07-24 15:29:50 -0500

    Updated

--- src/Exercise26.java
+++ src/Exercise26.java
@@ -6,7 +6,12 @@ public class Exercise26 {
     public static void main(String[] args){
         ArrayList<String> studentList = new ArrayList<String>();
         studentList.addAll(Arrays.asList("Bob", "Ross", "Teddy", "Pendegrass"));
+        StudentList(studentList);
+    }
+    public static void StudentList(ArrayList<String> studentList){
+
         System.out.println(studentList.get(2));
+
 
     }
 }
--- src/Exercise27.java
+++ src/Exercise27.java
@@ -5,6 +5,10 @@ public class Exercise27 {
     public static void main(String [] args){
         ArrayList<Integer> numbers = new ArrayList<Integer>();
         numbers.addAll(Arrays.asList(5,10,2,8,7));
-        System.out.println(numbers);
+        printIntArray(numbers);
+    }
+
+    public static void printIntArray(ArrayList<Integer> intArray){
+        System.out.println(intArray);
     }
 }
